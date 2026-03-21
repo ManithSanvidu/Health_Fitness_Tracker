cb00b3cc03080705bf1f112d7c19a02a28d82146
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-03-21 18:07:37 +0530

    Add files via upload

--- src/main/java/controller/RecommendationController.java
+++ src/main/java/controller/RecommendationController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://fitlife-foryou.netlify.app")
 public class RecommendationController {
 
     private final RecommendationService recommendationService;
