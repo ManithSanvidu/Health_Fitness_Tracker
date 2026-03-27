761da7723a437a8c3fb147584b10834684868d5f
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-03-27 11:05:44 +0530

    Add files via upload

--- java/config/CorsConfig.java
+++ java/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
             "http://localhost:3001",
             "http://127.0.0.1:3000",
             "http://127.0.0.1:3001",
-            "https://fitlife-foryou.netlify.app"
+            "https://health-fitness-tracker-three.vercel.app" 
     );
 
     @Bean
