7abdcce43c7f523e74e39fdf6f74d65bdbb76db0
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-03-29 12:56:31 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
-import { apiUrl } from "../config/api";
 import "./Auth.css";
 
 export default function Register() {
